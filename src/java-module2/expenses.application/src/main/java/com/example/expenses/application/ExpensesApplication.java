69419e8ec1e50b3981cdd0d1a5fac4bcb2b2609b
kimhaechang <khc9812121@gmail.com>
2024-07-11 18:20:48 +0900

    src: ch14 example update

---
+++ src/java-module2/expenses.application/src/main/java/com/example/expenses/application/ExpensesApplication.java
@@ -0,0 +1,10 @@
+package com.example.expenses.application;
+
+import com.example.expenses.readers.file.FileReader;
+
+public class ExpensesApplication {
+    public static void main(String[] args) {
+        FileReader httpReader = new FileReader();
+        System.out.println("Hello world!");
+    }
+}
